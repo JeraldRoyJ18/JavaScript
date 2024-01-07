f953dd0bc0c71f93accd286d16b685987dd5f889
JERALD ROY J <123557458+JeraldRoyJ18@users.noreply.github.com>
2024-01-07 16:24:45 +0000

    Data_types

---
+++ 01_basics/data_types.js
@@ -0,0 +1,16 @@
+console.log(
+    3 +
+    3
+)
+//null => stand_alone value
+
+//there are different documents official ones for js
+//even leaving mdn there is another one about ecma standards
+
+
+//symbol() => was kinda new data type for me
+//null => data type as well as type itself need to study more about this
+//typeof(null) , typeof(undefined) => need to study more about this
+
+//big int was nice mostly wont use it itseems
+//object => will study in the future.
