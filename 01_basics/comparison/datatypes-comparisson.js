1aae47fc2985181e58cedc0c815ab648701a6be9
JERALD ROY J <123557458+JeraldRoyJ18@users.noreply.github.com>
2024-01-09 05:17:44 +0000

    datatype-compatisson

---
+++ 01_basics/comparison/datatypes-comparisson.js
@@ -0,0 +1,16 @@
+
+console.log(null > 0)
+console.log(null == 0) //comparison and equal sign works differently
+console.log(null >= 0)
+
+
+//when converting null during comparison it converts it into zero
+//thats why null > 0 = false
+
+//primitves
+//7 types : strings , numbers , boolean  , null(empty) , undefined , symbol , bigInt
+
+//non - primitves(reference types)
+//arrays , object , functions
+//all the return type of the non - primitve stuff will be objects specifically for function
+//its function-object.
