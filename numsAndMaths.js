db30a80b3a1fdc316fec939f9ef3656f8139687f
JERALD ROY J <123557458+JeraldRoyJ18@users.noreply.github.com>
2024-01-10 10:22:55 +0000

    updated

---
+++ numsAndMaths.js
@@ -0,0 +1,7 @@
+
+
+const score = 300 //this is the general way of defining the number in js
+//but sometimes we want do it explicity saying that this should be number only 
+//in that case we use object like
+
+const balance = new Number(400)
